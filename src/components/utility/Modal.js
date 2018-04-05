b793410713566749dcf8813a241017d604a7acc6
Jack Durham <jackdurham11@gmail.com>
2018-04-05 14:36:40 +0100

    removing test

--- src/components/utility/Modal.js
+++ src/components/utility/Modal.js
@@ -7,9 +7,6 @@ const Modal = ({ user }) => {
       <div>
         <div className="card-body text-left">
           <p><span>Following: { user.following.length}</span> | <span>Followers: { user.followers.length}</span></p>
-          <div className="popup-content">
-            <span>TEST</span>
-          </div>
         </div>
       </div>} modal position="top center" closeOnDocumentClick>
     </Popup>
